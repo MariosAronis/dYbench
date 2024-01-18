e8a6ab2d7deb14ba26785ae30aec7be4641d5017
testuser-at-490752553772 <m_aronis@hotmail.com>
2024-01-19 00:47:49 +0200

    remove aws profile name

--- .github/scripts/bootstrap_network.sh
+++ .github/scripts/bootstrap_network.sh
@@ -6,7 +6,6 @@ VALUE="dymensionHub-node-1"
 
 get_instance_id () {
 InstanceID=`aws ec2 describe-instances \
-  --profile mariosee \
   --filters "Name=tag:Name,Values=$VALUE" \
             "Name=instance-state-name,Values=running" \
   --output json --query 'Reservations[*].Instances[*].{InstanceId:InstanceId}' | jq -r '.[] | .[] | ."InstanceId"'`   
@@ -17,7 +16,6 @@ BOOTSTRAP_NODE_EC2_ID=`get_instance_id`
 
 ssm_command () {
 COMMAND_ID=`aws ssm send-command \
-  --profile mariosee \
   --instance-ids "$BOOTSTRAP_NODE_EC2_ID" \
   --document-name "AWS-RunShellScript" \
   --cli-input-json $COMMAND | jq -r ' ."Command" | ."CommandId"'
@@ -27,7 +25,6 @@ echo $COMMAND_ID
 
 ssm_command_invocation () {
 SSM_RESULT=`aws ssm get-command-invocation \
-    --profile mariosee \
     --command-id $COMMAND_ID \
     --instance-id $BOOTSTRAP_NODE_EC2_ID | jq -r .`
 echo $SSM_RESULT
