223190e5ef0ddd72ae55e2c312dc64b03d286563
testuser-at-490752553772 <m_aronis@hotmail.com>
2024-01-18 21:19:50 +0200

    .GH actions config

---
+++ .github/scripts/cloud-init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y net-tools
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin gcc make jq unzip -y
+wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
+USER=`getent passwd 1000 | cut -d: -f1`
+tar -C /home/$USER -xzf go1.21.4.linux-amd64.tar.gz
+hostnamectl set-hostname "dybench-node-${count.index}"
+usermod -aG docker $USER
+echo "export PATH=$PATH:/home/$USER/go/bin" >> /home/$USER/.bashrc
+mkfs -t xfs /dev/nvme1n1 
+mkdir /data
+sudo mount /dev/nvme1n1 /data
+chown -R 1000:1000 /data
+chown -R 1000:1000 /home/$USER/go
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+git clone https://github.com/dymensionxyz/dymension.git
+cd dymension
---
+++ .github/scripts/deploy_dyhub.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#Number of validators in out privater dymensionHub
+VALIDATORS=$1
+AMI="ami-079167f081a690d5a"
+VALUE="dymensionHub-node"
+
+get_vpc_id () {
+VPC_ID=`aws ec2 describe-vpcs \
+    --profile mariosee \
+    --filters "Name=tag:Name, Values=dybench" \
+    --output json --query 'Vpcs[*].{VpcId:VpcId}' | jq '. [] | ."VpcId"'`
+echo $VPC_ID
+}
+
+get_subnet_id () {
+SUBNET_ID=`aws ec2 describe-subnets \
+    --profile mariosee \
+    --filters "Name=tag:Name, Values=dybench-priv" \
+    --output json --query 'Subnets[*].{SubnetId:SubnetId}' | jq '. [] | ."SubnetId"'`
+echo $SUBNET_ID
+}
+
+get_sg_id () {
+SG_ID=`aws ec2 describe-security-groups \
+    --profile mariosee \
+    --filters "Name=tag:Name, Values=dybench-private" \
+    --output json --query 'SecurityGroups[*]'.{GroupId:GroupId} | jq '. [] | ."GroupId"'`
+echo $SG_ID
+}
+
+create_node () {
+aws ec2 run-instances \
+  --user-data "file://.github/scripts/cloud-init.sh" \
+  --image-id $AMI \
+  --count 1 \
+  --instance-type t3.2xlarge \
+  --key-name dybench \
+  --security-group-ids $SG_ID \
+  --subnet-id $SUBNET_ID \
+  --block-device-mappings "[{\"DeviceName\":\"/dev/sda1\",\"Ebs\":{\"VolumeSize\":500,\"DeleteOnTermination\":true}}]" \
+  --iam-instance-profile Arn=$InstanceProfile \
+  --instance-initiated-shutdown-behavior terminate \
+  --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value="$VALUE-$INDEX"}]" \
+  --metadata-options "InstanceMetadataTags=enabled" 
+}
+
+get_node_id () {
+    pass
+}
+
+delete_node () {
+    pass
+}
+
+#FETCH INFRA DETAILS
+VPC_ID=`get_vpc_id`
+SUBNET_ID=`get_subnet_id`
+SG_ID=`get_sg_id`
+
+#SPIN DIMENSION HUB NODES
+for INDEX in $(seq $VALIDATORS); do
+    create_node
+done
