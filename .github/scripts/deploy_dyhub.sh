e4834c15719a0f7e63161e89c0793034129e6943
testuser-at-490752553772 <m_aronis@hotmail.com>
2024-01-18 21:26:44 +0200

    remove aws profile from aws cli commands

--- .github/scripts/deploy_dyhub.sh
+++ .github/scripts/deploy_dyhub.sh
@@ -7,7 +7,6 @@ VALUE="dymensionHub-node"
 
 get_vpc_id () {
 VPC_ID=`aws ec2 describe-vpcs \
-    --profile mariosee \
     --filters "Name=tag:Name, Values=dybench" \
     --output json --query 'Vpcs[*].{VpcId:VpcId}' | jq '. [] | ."VpcId"'`
 echo $VPC_ID
@@ -15,7 +14,6 @@ echo $VPC_ID
 
 get_subnet_id () {
 SUBNET_ID=`aws ec2 describe-subnets \
-    --profile mariosee \
     --filters "Name=tag:Name, Values=dybench-priv" \
     --output json --query 'Subnets[*].{SubnetId:SubnetId}' | jq '. [] | ."SubnetId"'`
 echo $SUBNET_ID
@@ -23,10 +21,17 @@ echo $SUBNET_ID
 
 get_sg_id () {
 SG_ID=`aws ec2 describe-security-groups \
-    --profile mariosee \
     --filters "Name=tag:Name, Values=dybench-private" \
     --output json --query 'SecurityGroups[*]'.{GroupId:GroupId} | jq '. [] | ."GroupId"'`
 echo $SG_ID
+}
+
+get_iam_instance_profile () {
+InstanceProfile=`aws iam get-instance-profile \
+  --instance-profile-name dybenchnode-profile \
+  --output text \
+  --query 'InstanceProfile.Arn'`
+echo $InstanceProfile
 }
 
 create_node () {
@@ -57,6 +62,7 @@ delete_node () {
 VPC_ID=`get_vpc_id`
 SUBNET_ID=`get_subnet_id`
 SG_ID=`get_sg_id`
+InstanceProfile=`get_iam_instance_profile`
 
 #SPIN DIMENSION HUB NODES
 for INDEX in $(seq $VALIDATORS); do
