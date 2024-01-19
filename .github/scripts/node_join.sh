445b8af2ad586974f5d04b89a78ca964ec0f5016
testuser-at-490752553772 <m_aronis@hotmail.com>
2024-01-19 23:31:46 +0200

    node_join

--- .github/scripts/node_join.sh
+++ .github/scripts/node_join.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -e
+
 su ubuntu -c 'aws s3 cp s3://dybenchd-binaries/dymd $HOME/go/bin'
 chmod +x /home/ubuntu/go/bin/dymd
-su ubuntu -c 'dymd start'
+su ubuntu -c '$HOME/go/bin/dymd start'
 su ubuntu -c 'aws s3 cp s3://dybenchd-binaries/genesis.json /home/ubuntu/.dymension/config/'
