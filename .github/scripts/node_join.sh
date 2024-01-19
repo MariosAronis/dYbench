cad64393d9606fd1f47de604ac3f2f406bd4c6c2
testuser-at-490752553772 <m_aronis@hotmail.com>
2024-01-19 23:45:22 +0200

    node_join

--- .github/scripts/node_join.sh
+++ .github/scripts/node_join.sh
@@ -4,5 +4,5 @@ set -e
 
 su ubuntu -c 'aws s3 cp s3://dybenchd-binaries/dymd $HOME/go/bin'
 chmod +x /home/ubuntu/go/bin/dymd
-su ubuntu -c '$HOME/go/bin/dymd start'
-su ubuntu -c 'aws s3 cp s3://dybenchd-binaries/genesis.json /home/ubuntu/.dymension/config/'
+su ubuntu -c 'mkmdir -p $HOME/.dymension/config/; aws s3 cp s3://dybenchd-binaries/genesis.json $HOME/.dymension/config/'
+su ubuntu -c '$HOME/go/bin/dymd start --json-rpc.enable > $HOME/.dymension/dymd.log 2>&1 &'
