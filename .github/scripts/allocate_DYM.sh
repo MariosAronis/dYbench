b48cccbf3593f8b37ceaa3d3fd92ac6515751b5c
testuser-at-490752553772 <m_aronis@hotmail.com>
2024-01-22 16:04:34 +0200

    allocate DYM

--- .github/scripts/allocate_DYM.sh
+++ .github/scripts/allocate_DYM.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 su ubuntu -c 'aws s3 cp s3://dybenchd-binaries/accounts.json $HOME/accounts.json'
 
 SCRIPT=$( cat <<'EOF'
 for address in `jq -r ".validators_accounts | .[]" <<< $(cat $HOME/accounts.json)`; do
-echo $address
-$HOME/go/bin/dymd tx bank send local-user $address 1000000000000000000000000000udym --chain-id dymension_999-9 --from local-user --fees 20000000000000udym -y
+SEQUENCE=$($HOME/go/bin/dymd query account \
+`$HOME/go/bin/dymd keys show "local-user" \
+--keyring-backend test -a` \
+--output json | jq -r '.base_account | .sequence');
+echo $SEQUENCE;
+$HOME/go/bin/dymd tx bank send local-user \
+$address 1000000000000000000000000000udym \
+--chain-id dymension_999-9 \
+--from "local-user" \
+--fees 20000000000000udym \
+--sequence $SEQUENCE \
+--yes;
 done
 EOF
 )
